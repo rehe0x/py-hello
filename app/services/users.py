16ceff52698a400a8f557e609bcb1fbf246258ea
xiech <xiechanghong@everimaging.com>
2018-12-29 18:29:54 +0800

    日常

--- app/services/users.py
+++ app/services/users.py
@@ -1,6 +1,41 @@
 from app import db
+import requests
+import time
+from common import FileUtils
 
-def find_user():
-    user = db.session.execute('select * from users')
+def find_user(pageNo,pageSize):
+    no = pageNo if pageNo > 0 else 1;
+    size = pageSize * (no - 1);
+
+    params,dict_params = [],{}
+    dict_params['pageNo'] = size
+    dict_params['pageSize'] = pageSize
+   
+    params.append(dict_params)
+
+    user = db.session.execute('select sticker_id,thumb_url,mediumPic,sticker_url from t_sticker where state=0 limit :pageNo,:pageSize',params)
     
     return user.fetchall()
+
+def handle_sticker_400(threadName,pageNo,pageSize,url):
+    re = find_user(pageNo,pageSize)
+    if len(re)==0:
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>null"+str(pageNo))
+    else:
+        for i in re:
+            try:
+                r = requests.get(url + i[3])
+                #r1 = requests.get(url='http://dict.baidu.com/s', params={'wd': 'python'})
+                if r.status_code == 404:
+                    #print(r.status_code)
+                    FileUtils.in_file('sticker_error.txt',i[0]+'==='+i[3])
+                    print ("%s: %s" % (threadName, time.ctime(time.time())))
+                else:
+                    r1 = requests.get(url + i[1])
+                    if r1.status_code == 404:
+                        FileUtils.in_file('sticker_error.txt',i[0]+'==='+i[1])
+                        print ("%s: %s" % (threadName, time.ctime(time.time())))
+
+            except Exception as e:
+                 FileUtils.in_file('sticker_error.txt',i[0]+'==='+i[3])
+                 print('error>>>>>>>>>>>',e)
--- main.py
+++ main.py
@@ -6,8 +6,14 @@ import re
 import requests
 import json
 from app import users
+from common import FileUtils
+import threading
+import time
+
 
 STATIC_URL = 'https://pub-static.haozhaopian.net'
+THREAD_NUM = 20
+PAGE_SZIE = 100
 
 app = Flask(__name__)
 
@@ -20,20 +26,59 @@ def index():
 def hello():
     return 'Hello,World!'
 
+exitFlag = 0
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter,pageNo):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.pageNo = pageNo
+    def run(self):
+        print ("开始线程：" + self.name)
+        users.handle_sticker_400(self.name,self.pageNo,PAGE_SZIE,STATIC_URL)
+        print ("退出线程：" + self.name)
+
+# 为线程定义一个函数
+def print_time(threadName, delay, counter):
+    while counter:
+        if exitFlag:
+            threadName.exit()
+        time.sleep(delay)
+        print ("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
 if __name__ == '__main__':
-    r = requests.get(STATIC_URL + '/assets/res/sticker/d63ce32c-3397-4b01-9a81-56d2f5bebc291.svg')
-    #r1 = requests.get(url='http://dict.baidu.com/s', params={'wd': 'python'})
-    if r.status_code == 404:
-        print(r.status_code)
-    ret = users.find_user()
-    print(ret)
-    for i in ret:
-        print(i[0])
-    # dict1 = {}
-    # dict1['id'] = "This is one"
-    # dict1['url'] = "This is two"
-    # f = open('sticker.json','a',encoding = 'utf-8')
-    # json.dump(dict1,f)
-    # re = json.loads(f.read())
-    # print(re[1]['id'])
+    #users.handle_sticker_400('23',54,PAGE_SZIE,STATIC_URL)
+    # r = requests.get(STATIC_URL + '/assets/res/sticker/d63ce32c-3397-4b01-9a81-56d2f5bebc291.svg')
+
+    #====================================================================
+    # pageNo = FileUtils.read_file_last('pageNo.txt')
+    # threadN = 0;
+    # while threadN < THREAD_NUM:
+    #     pageNo = pageNo + 1 
+    #     threadN = threadN + 1            
+    #     with open('pageNo.txt', 'r',encoding = 'utf-8') as p:
+    #         listNo = p.read()
+    #         if str(pageNo) not in listNo:
+    #             FileUtils.in_file('pageNo.txt',pageNo)
+    #             #创建新线程
+    #             thread1 = myThread(1, "Thread-"+str(threadN)+"-"+str(pageNo), threadN,pageNo)
+    #             thread1.start()
+    #         else:
+    #             print('>>>>>>>>>>>>>>>chongfu')
+
+
+    #=======================================================================
+    headers={
+        'Authorization':''
+    }
+    # rep = requests.post(url='http://52.80.15.237:8025/sticker/syncResources', params={'ids': 'fb23a40a-eb70-4669-97ce-9d27aba31621','syncState':'01'},headers=headers)  
+    # print(rep.text)      
+    with open('sticker_error.txt', 'r',encoding = 'utf-8') as p:
+        re = p.readlines()
+        for i in re:
+            id = i.split('===')[0]
+            rep = requests.post(url='http://:8025/sticker/syncResources', params={'ids': id,'syncState':'01'},headers=headers)  
+            print(rep.text)      
     #app.run()
